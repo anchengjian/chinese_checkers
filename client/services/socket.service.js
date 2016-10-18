b24e20ae3c194e4c85cbcd7a703bdaba4565a680
anchengjian <anchengjian@gmail.com>
2016-10-18 18:37:35 +0800

    feat: 开始优化socket，加房间

--- client/services/socket.service.js
+++ client/services/socket.service.js
@@ -3,10 +3,10 @@ import { serverHost } from 'CONFIG/app.config';
 const socket = io(serverHost);
 export default socket;
 
-socket.emit('hello', '233');
-socket.on('word', function(msg) {
-  console.log('握手成功。message: ' + msg);
-});
+// socket.emit('hello', '233');
+// socket.on('word', function(msg) {
+//   console.log('握手成功。message: ' + msg);
+// });
 
 const SELF_MOVE = 'SELF_MOVE';
 const OTHER_MOVE = 'OTHER_MOVE';
@@ -22,3 +22,23 @@ export function otherPlayerMove(cb) {
     if (typeof cb === 'function') cb(data.player, data.move);
   });
 }
+
+// 加入房间
+socket.on('connect', function() {
+  socket.emit('join', 'userName');
+});
+
+// 监听消息
+socket.on('msg', function(userName, msg) {
+
+});
+
+// 监听系统消息
+socket.on('sys', function(sysMsg, users) {
+
+});
+// 发送消息
+// socket.send(msg);
+
+// 退出房间
+// socket.emit('leave');
--- server/socket.js
+++ server/socket.js
@@ -8,29 +8,65 @@ class Socket {
     // 先启一个socket服务
     this.io = require('socket.io')(app.httpServer);
 
+    // 房间信息
+    this.roomInfo = {};
+
+    this.socketRunWithEvents();
+  }
+  socketRunWithEvents() {
     // 建立长连接后开始执行逻辑
     this.io.on('connection', function(socket) {
       let socketId = socket.id;
       log('建立了一个socket的长连接哦', socketId);
 
-      // socket._events = {
-      //   'test': function(from, msg) { }
-      // };
-
-      socket.on('disconnect', () => log('断开了一个连接', 'socketId=>', socketId));
       socket.on('error', () => log('一个错误了', 'socketId=>', socketId));
 
       // 无聊,先握手
-      socket.on('hello', (msg) => {
-        log('一个官方的正式握手');
-        socket.emit('word', '666');
+      // socket.on('hello', (msg) => {
+      //   log('一个官方的正式握手');
+      //   socket.emit('word', '666');
+      // });
+
+      // socket.on('SELF_MOVE', (data) => {
+      //   socket.broadcast.emit('OTHER_MOVE', data);
+      // });
+
+      let user = '';
+      let roomID = '';
+
+      socket.on('join', (data) => {
+        user = data.user;
+        roomID = data.roomID;
+        if (!user || !roomID) return;
+
+        // 将用户昵称加入房间名单中
+        if (!this.roomInfo[roomID]) this.roomInfo[roomID] = [];
+        this.roomInfo[roomID].push(data);
+
+        socket.join(roomID); // 加入房间
+        // 通知房间内人员
+        socketIO.to(roomID).emit('sys', user + '加入了房间', this.roomInfo[roomID]);
       });
 
-      socket.on('SELF_MOVE', (data) => {
-        // log('收到一个玩家的运动', data);
-        socket.broadcast.emit('OTHER_MOVE', data);
-        // log('转发了消息', data);
+      socket.on('leave', () => socket.emit('disconnect'));
+
+      socket.on('disconnect', () => {
+        log('断开了一个连接', 'socketId=>', socketId);
+        // 从房间名单中移除
+        var index = this.roomInfo[roomID].indexOf(user);
+        if (index >= 0) this.roomInfo[roomID].splice(index, 1);
+
+        socket.leave(roomID); // 退出房间
+        socketIO.to(roomID).emit('sys', user + '退出了房间', this.roomInfo[roomID]);
       });
+
+      // 接收用户消息,发送相应的房间
+      socket.on('message', (msg) => {
+        // 验证如果用户不在房间内则不给发送
+        if (this.roomInfo[roomID].indexOf(user) === -1) return;
+        socketIO.to(roomID).emit('msg', user, msg);
+      });
+
     });
   }
 }
